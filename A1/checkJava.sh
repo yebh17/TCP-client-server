02206c3b9406e423cfc10984e3d5d791eff6010e
yebh17 <sunnyrebels0@gmail.com>
2020-12-13 18:00:01 +0100

    Set java installation error message convey that, to install java 'jdk' type.

--- A1/checkJava.sh
+++ A1/checkJava.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
 if type -p java; then
-    echo found java executable in PATH
+    echo OK, found java executable in PATH
     _java=java
 elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo Found Java executable in JAVA_HOME     
-    _java="$JAVA_HOME/bin/java"
+    echo OK, found Java executable in JAVA_HOME     
+     _java="$JAVA_HOME/bin/java"
 else
-    echo "No Java found! Please install Java using 'sudo apt install default-jre'"
+    echo "ERROR, no Java found! Please Install 'default-jdk' Java"
 fi
 
 if [[ "$_java" ]]; then
     version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
     echo version "$version"
     if [[ "$version" > "1.5" ]]; then
-        echo version is more than 1.5
+        echo OK, Java version is more than 1.5
     else         
-        echo version is less than 1.5
+        echo ERROR, Java version is less than 1.5
     fi
-fi
-
-if type -p javac; then
-    echo found javac executable in PATH
-else
-    echo "No Javac found! Please Install Javac using 'sudo apt install default-jdk'"
 fi
