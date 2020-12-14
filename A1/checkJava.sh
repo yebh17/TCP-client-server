293250e7c16a020bbd74b88de5f8635f4bdab635
yebh17 <sunnyrebels0@gmail.com>
2020-12-14 18:42:57 +0100

    Remove java version compatibility check and only check for java installation

--- A1/checkJava.sh
+++ A1/checkJava.sh
@@ -9,13 +9,3 @@ elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
 else
     echo "ERROR, no Java found! Please Install 'default-jdk' Java"
 fi
-
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
-    if [[ "$version" > "1.5" ]]; then
-        echo OK, Java version is more than 1.5
-    else         
-        echo ERROR, Java version is less than 1.5
-    fi
-fi
